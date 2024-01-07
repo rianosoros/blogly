d432d463b7b9879fb0a9bda04ecdc1b33085b57d
Rian <rgillard@nuccimedical.com>
2024-01-07 18:42:36 -0500

    Add files via upload

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -20,6 +21,25 @@ class User(db.Model):
 
         return f"{self.first_name} {self.last_name}"
 
+class Post(db.Model):
+    """Blog post."""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    @property
+    def friendly_date(self):
+        """Return nicely-formatted date."""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
 
 def connect_db(app):
 
